b6ed48936e498ed54384570a808f78f0bdf623c4
DyeffersonAz <dyeffersonazevedo@hotmail.com>
2018-03-26 17:40:34 -0300

    Criação do arquivo da tela principal

--- JavaUrn/src/com/dyeffersonaz/javaurn/main/TelaInicial.java
+++ JavaUrn/src/com/dyeffersonaz/javaurn/main/TelaInicial.java
@@ -1,9 +1,14 @@
+package com.dyeffersonaz.javaurn.main;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JButton;
 
 public class TelaInicial extends JFrame {
 
@@ -29,12 +34,21 @@ public class TelaInicial extends JFrame {
 	 * Create the frame.
 	 */
 	public TelaInicial() {
+		setTitle("Urna Eletrônica - JAVAURN");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JLabel lblUrnaEletrnica = new JLabel("Urna Eletrônica - JAVAURN");
+		lblUrnaEletrnica.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblUrnaEletrnica.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lblUrnaEletrnica, BorderLayout.NORTH);
+		
+		JButton btnNovaEleio = new JButton("Nova Eleição");
+		contentPane.add(btnNovaEleio, BorderLayout.CENTER);
 	}
 
 }
